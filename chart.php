3f2bcdf9908436e3dea4e87556ca7a2772d1076e
amirholakoo <amirholakoo@gmail.com>
2023-11-17 22:48:39 -0800

    Update chart.php

--- chart.php
+++ chart.php
@@ -50,46 +50,96 @@ $conn->close();
 <body>
 
 <div style="width:75%;">
-    <canvas id="fermentationChart"></canvas>
+    <h2>Temperature</h2>
+    <canvas id="temperatureChart"></canvas>
+    <h2>Humidity</h2>
+    <canvas id="humidityChart"></canvas>
+    <h2>CO2 Level</h2>
+    <canvas id="co2Chart"></canvas>
+    <h2>Alcohol Level</h2>
+    <canvas id="alcoholChart"></canvas>
 </div>
 
 <script>
-    var ctx = document.getElementById('fermentationChart').getContext('2d');
-    var fermentationChart = new Chart(ctx, {
+    var ctxTemp = document.getElementById('temperatureChart').getContext('2d');
+    var temperatureChart = new Chart(ctxTemp, {
         type: 'line',
         data: {
             labels: <?php echo json_encode(array_reverse($timestamps)); ?>,
-            datasets: [
-                {
-                    label: 'Temperature',
-                    data: <?php echo json_encode(array_reverse($temperatures)); ?>,
-                    borderColor: 'red',
-                    fill: false
-                },
-                {
-                    label: 'Humidity',
-                    data: <?php echo json_encode(array_reverse($humidities)); ?>,
-                    borderColor: 'blue',
-                    fill: false
-                },
-                {
-                    label: 'CO2 Level',
-                    data: <?php echo json_encode(array_reverse($co2_levels)); ?>,
-                    borderColor: 'green',
-                    fill: false
-                },
-                {
-                    label: 'Alcohol Level',
-                    data: <?php echo json_encode(array_reverse($alcohol_levels)); ?>,
-                    borderColor: 'purple',
-                    fill: false
-                }
-            ]
+            datasets: [{
+                label: 'Temperature',
+                data: <?php echo json_encode(array_reverse($temperatures)); ?>,
+                borderColor: 'red',
+                fill: false
+            }]
         },
         options: {
             scales: {
                 y: {
-                    beginAtZero: true
+                    beginAtZero: false
+                }
+            }
+        }
+    });
+
+    var ctxHumidity = document.getElementById('humidityChart').getContext('2d');
+    var humidityChart = new Chart(ctxHumidity, {
+        type: 'line',
+        data: {
+            labels: <?php echo json_encode(array_reverse($timestamps)); ?>,
+            datasets: [{
+                label: 'Humidity',
+                data: <?php echo json_encode(array_reverse($humidities)); ?>,
+                borderColor: 'blue',
+                fill: false
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: false
+                }
+            }
+        }
+    });
+
+    var ctxCo2 = document.getElementById('co2Chart').getContext('2d');
+    var co2Chart = new Chart(ctxCo2, {
+        type: 'line',
+        data: {
+            labels: <?php echo json_encode(array_reverse($timestamps)); ?>,
+            datasets: [{
+                label: 'CO2 Level',
+                data: <?php echo json_encode(array_reverse($co2_levels)); ?>,
+                borderColor: 'green',
+                fill: false
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: false
+                }
+            }
+        }
+    });
+
+    var ctxAlcohol = document.getElementById('alcoholChart').getContext('2d');
+    var alcoholChart = new Chart(ctxAlcohol, {
+        type: 'line',
+        data: {
+            labels: <?php echo json_encode(array_reverse($timestamps)); ?>,
+            datasets: [{
+                label: 'Alcohol Level',
+                data: <?php echo json_encode(array_reverse($alcohol_levels)); ?>,
+                borderColor: 'purple',
+                fill: false
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: false
                 }
             }
         }
