7a8fd99db72a4d446c5bf6552c9cbd7497583da2
amirholakoo <amirholakoo@gmail.com>
2023-11-17 22:39:57 -0800

    Update post-data.php

--- post-data.php
+++ post-data.php
@@ -2,8 +2,12 @@
 $servername = "localhost";
 $username = "username"; // replace with your database username
 $password = "password"; // replace with your database password
-$dbname = "mead_monitoring";
-$tableName = "Lavender01";
+
+$dbname = "mead_monitoring"; // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+///////////////////////////////////////////////////////////////////
+$tableName = "Lavender01"; // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<       Change with new database every time
+///////////////////////////////////////////////////////////////////
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -20,32 +24,31 @@ $co2_level = $_POST['co2_level'];
 $alcohol_level = $_POST['alcohol_level']; // assuming you're sending this data
 
 // Check if the table exists, and if not, create it
-	$table_check = "SHOW TABLES LIKE '$tableName'";
-	$result = $conn->query($table_check);
-	if (!$result) {
-	    die("Query failed: " . $conn->error);
-	}
+$table_check = "SHOW TABLES LIKE '$tableName'";
+$result = $conn->query($table_check);
+if (!$result) {
+    die("Query failed: " . $conn->error);
+}
 
-	if ($result->num_rows == 0) {
-	  $create_table = "CREATE TABLE '$tableName' (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-            temperature FLOAT,
-            humidity FLOAT,
-            co2_level FLOAT,
-            alcohol_level FLOAT
-	)";
+if ($result->num_rows == 0) {
+    $create_table = "CREATE TABLE `$tableName` (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+        temperature FLOAT,
+        humidity FLOAT,
+        co2_level FLOAT,
+        alcohol_level FLOAT
+    )";
 
-
-	  if ($conn->query($create_table) === TRUE) {
-	    echo "Table 'weather_data' created successfully";
-	  } else {
-	    echo "Error creating table: " . $conn->error;
-	  }
-	}
+    if ($conn->query($create_table) === TRUE) {
+        echo "Table $tableName created successfully";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
+}
 
 // Prepare and bind
-$stmt = $conn->prepare("INSERT INTO '$tableName' (temperature, humidity, co2_level, alcohol_level) VALUES (?, ?, ?, ?)");
+$stmt = $conn->prepare("INSERT INTO `$tableName` (temperature, humidity, co2_level, alcohol_level) VALUES (?, ?, ?, ?)");
 $stmt->bind_param("dddd", $temperature, $humidity, $co2_level, $alcohol_level);
 
 // Execute the statement
